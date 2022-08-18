37219474797a68f56e6c923339d20675acadb72b
Kevin Bell <bellKevin@pm.me>
2022-08-17 20:23:28 -0600

    Update BinarySearchDriver.java

--- binarySearchDriver/src/binarysearchdriver/BinarySearchDriver.java
+++ binarySearchDriver/src/binarysearchdriver/BinarySearchDriver.java
@@ -4,7 +4,6 @@ package binarysearchdriver;
 public class BinarySearchDriver {
 
     public static void main(String[] args) {
-        // TODO code application logic here
         int[] array = new int[]{-7, 3, 5, 8, 12, 16, 23, 33, 55};
         System.out.println(BinarySearch.binarySearch(array, 0, (array.length - 1), 23));
         System.out.println(BinarySearch.binarySearch(array, 0, (array.length - 1), 4));
